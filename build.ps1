23cf472cec31b7da276f71d62731bca17d0a463f
JBob <91869208+NerdyGamers@users.noreply.github.com>
2025-09-05 06:46:35 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,8 +2,8 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
 if (-not (Get-Command dotnet -ErrorAction SilentlyContinue)) {
-    Invoke-WebRequest https://dot.net/v1/dotnet-install.ps1 -OutFile dotnet-install.ps1
-    & powershell -ExecutionPolicy Bypass -File ./dotnet-install.ps1 -Version 8.0.100 -InstallDir "$PSScriptRoot/.dotnet"
+    Invoke-WebRequest https://builds.dotnet.microsoft.com/dotnet/scripts/v1/dotnet-install.ps1 -OutFile dotnet-install.ps1
+    & powershell -ExecutionPolicy Bypass -File ./dotnet-install.ps1 -Version 8.0.413 -InstallDir "$PSScriptRoot/.dotnet"
     $env:PATH = "$PSScriptRoot/.dotnet;" + $env:PATH
 }
 
